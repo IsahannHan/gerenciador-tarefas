6af0c3b91571027068e58d84994c4bdad65256ea
isahann <isahanacleto@gmail.com>
2022-12-02 22:44:46 -0300

    Auto-generated commit [ci skip]

--- src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import * as M from 'materialize-css'
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass'],
-})
-export class AppComponent {
-}
